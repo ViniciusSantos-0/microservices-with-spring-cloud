0d7cd8d87693eca5e08411c24f8253762a13bfde
Vinicius Santos <viniciushacrinha1@gmail.com>
2021-12-08 21:54:23 -0300

    creating the shopping cart and classes

---
+++ shopping-cart/src/main/java/com/vinicius/shoppingcart/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package com.vinicius.shoppingcart.repository;
+
+import com.vinicius.shoppingcart.model.Cart;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CartRepository extends CrudRepository<Cart, Integer> {
+
+    Cart findByCustomerId(Integer customerId);
+
+}
